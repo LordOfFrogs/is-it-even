6ea644214b4e6b115e6c09d9f9314a683e9ba93a
Tori <ndtech260@gmail.com>
2024-04-24 12:34:26 -0400

    started isiteven page

---
+++ app/it-it-even/page.tsx
@@ -0,0 +1,12 @@
+function isEven(number: number) {
+    const request = new Request("https://api.isevenapi.xyz/api/${number}/");
+    
+}
+
+export default function Home() {
+    return (
+        <main>
+            
+        </main>
+    );
+}
