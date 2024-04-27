46decfcc2df9a9d0679f9a99e988312e5ca9369c
LordOfFrogs <ndtech260@gmail.com>
2024-04-27 14:48:45 -0400

    use digest for errors

--- app/is-it-even/[num]/error.tsx
+++ app/is-it-even/[num]/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
     return (
         <div className="flex static flex-col items-start justify-center">
             <p className="text-lg font-bold">Something went wrong:</p>
-            <p className="text-lg font-medium font-mono bg-neutral-800 px-2 rounded">{'"'+error.message+'"'}</p>
+            <p className="text-lg font-medium font-mono bg-neutral-800 px-2 rounded">{'"'+error.message+'"\n Digest: '+error.digest}</p>
         </div>
     )
 }
